d95006ac5196cd5bf05f69be0f5f12f7a0b3a3fb
Quentin Khoo <quentinkhoo@outlook.com>
2019-08-27 00:00:57 +0800

    Initial Commit

---
+++ Tutorial 1/QuentinKhoo_Tut1b/app/src/main/java/com/quentin/is3261/quentinkhoo_tut1b/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.quentin.is3261.quentinkhoo_tut1b
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
